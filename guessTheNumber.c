b51f22855e208d974acbda2672b06dc8116edc12
KrishNayak421 <krishnayak421@gmail.com>
2024-10-23 18:37:46 +0530

    Changes in programs

--- guessTheNumber.c
+++ guessTheNumber.c
@@ -1,1 +1,15 @@
 /*C program mini game for guessing number between 1 to 100*/
+#include<stdio.h>
+
+int main()
+{
+    int no = 69;
+    int n;
+    printf("enter a number between 1 to 100\n");
+    scanf("%d",&n);
+
+    if(n == no)
+    printf("Right guess\n");
+    else
+    printf("Not Right guess\n");
+}
