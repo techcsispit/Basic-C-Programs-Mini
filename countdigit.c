666a369533e56561a09d4e382f4e911b6c8da5a4
Vatsal Panchal <vatsalpanchal2006@gmail.com>
2024-10-23 19:29:30 +0530

    This code calculates the number of digits in a given number.

--- countdigit.c
+++ countdigit.c
@@ -1,1 +1,19 @@
 //C program to count number of digits in a number.
+#include <stdio.h>
+
+int main()
+{
+    int i, n, y;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    for (i = 1;; i++)
+    {
+        n = n / 10;
+        if (n == 0)
+        {
+            break;
+        }
+    }
+    printf("The number of digits is/are %d", i);
+    return 0;
+}
