6948f5de50e940119b37c6748270f02912f24aab
SahuAyush26 <ayush.sahu24@spit.ac.in>
2024-10-23 19:28:03 +0530

    formatted the document, so that it looks better

--- Predefined_Macros.c
+++ Predefined_Macros.c
@@ -4,10 +4,10 @@ int main()
 {
     printf("Predefined Macros :\n");
 
-    printf("Current time :       %s\n",__TIME__);
-    printf("Current file Name :  %s\n",__FILE__);
-    printf("Current Date :       %s\n",__DATE__);
-    printf("Line number is:      %d\n",__LINE__); 
+    printf("Current time :          %s\n",__TIME__);
+    printf("Current file Name :     %s\n",__FILE__);
+    printf("Current Date :          %s\n",__DATE__);
+    printf("Line number is:         %d\n",__LINE__); 
     
     return 0;
 }
