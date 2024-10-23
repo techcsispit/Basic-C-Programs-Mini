1d4618678e15e7eb790e3aa9e1ebc3c533364b10
neeravj06@gmail.com <neeravj06@gmail.com>
2024-10-23 19:31:31 +0530

    Even moreeeeeeeee commits

--- countdigit.c
+++ countdigit.c
@@ -1,1 +1,14 @@
-//C program to count number of digits in a number.
+//C program to count number of digits in a number
+#include<stdio.h>
+void main()
+{
+    int n, count=0;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        n = n/10;
+        count++;
+    }
+    printf("The number of digits are:%d\n", count);
+}
