e3b17d8f21979c2f9b7f9f66544ea889d6858516
het salot <hetsalot1410@gmail.com>
2024-10-23 19:24:27 +0530

    quadratic

--- quadratic.c
+++ quadratic.c
@@ -1,1 +1,13 @@
 //C program  to find roots of a quadratic equation.
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the coefficient of x^2, x and constant respecitvly");
+    scanf("%d %d %d",&a,&b,&c);
+    int r1=(-b+sqrt(b*b-4*a*c))/(2*a);
+    int r2=(-b-sqrt(b*b-4*a*c))/(2*a);
+    printf("Roots are %d and %d",r1,r2);
+    return 0;
+}
