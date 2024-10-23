90cc28dc4a265b5939a183597561db2ae5d80eb8
thedevanshimehta <dvanshi613@gmail.com>
2024-10-23 19:34:21 +0530

    cpp file for print_number_Line_by_Line_Sum_and_total

--- print_number_Line_by_Line_Sum_and_total.c
+++ print_number_Line_by_Line_Sum_and_total.c
@@ -6,20 +6,45 @@
 //output be like :- 1
 //                  2
 //                  3
-void line_by_line(int no){
+void line_by_line(int x);
+void line_by_line(int a){
+    int a, b,c;
+    do{
+        b = a%10;
+        printf("%d\n", b);
+        a = a/10;
+    }
+    while (a!=0);
+    
     
 }
 
 //Function to print Sum of the entered digit.
 // Example :- 123 = 6
+void sum_digit(int x);
 void sum_digit(int no){
-    
+    int a, b,c; a=no; int s=0; c=no;
+    do{
+        b = a%10;
+        a = a/10;
+        s=s+b;
+    }
+    while (a!=0);
+    printf("%d = %d\n",c,s);
 }
 
 //Function to print total of the entered digit.
 // Example :- 123 = 3
+void total_digit(int x);
 void total_digit(int no){
-    
+    int a, b,c; a=no; int s=0; c=no;
+    do{
+        b = a%10;
+        a = a/10;
+        s++;
+    }
+    while (a!=0);
+    printf("%d = %d",c,s);
 }
 int main()
 {
@@ -32,4 +57,5 @@ int main()
     sum_digit(no);
     
     total_digit(no);
+    return 0;
 }
