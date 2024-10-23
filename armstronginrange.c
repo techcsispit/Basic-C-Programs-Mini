e49c94bc49f64ca4843f4bf403b7e470d1629460
KrishNayak421 <krishnayak421@gmail.com>
2024-10-23 18:55:31 +0530

    armstrong in range completed

--- armstronginrange.c
+++ armstronginrange.c
@@ -1,4 +1,43 @@
 /*C program to display armstrong number
 between 1 to 1000*/
+#include<stdio.h>
+int armstrong(int n);
+int main()
+{
+
+    for(int i = 1; i <= 1000; i ++)
+    {
+        if(armstrong(i) == 1)
+        printf("%d", i);
+        else 
+        printf("No");
+    }
+    return 0;
+}
 
 
+
+
+
+int armstrong(int n)
+{
+    int b = n;
+    int sum = 0;
+    while(b>0)
+    {
+        int m;
+        if(b > 10){
+            m = b%10;
+        }
+        else{
+           m = b; 
+        }
+        b = b/10;
+        sum = sum+(m*m*m);
+    }
+
+    if(sum == n)
+    return 1;
+    else
+    return 0;
+}
