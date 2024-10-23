0b2df7308f95bab485de984f1a041f255de66104
prashamgosar <prashamgosar@gmail.com>
2024-10-23 19:30:32 +0530

    commit

--- palindrome.c
+++ palindrome.c
@@ -1,2 +1,19 @@
 //C program to check a number whether it is palindrome or not.
-
+#include<stdio.h>
+void main()
+{
+int n,rev=0,r,pal;
+printf("Enter any integer number\n");
+scanf("%d", &n);
+pal=n;
+while(n!=0)
+{
+ r=n%10;
+ rev=rev*10+r;
+ n=n/10;
+ }
+ if(rev==pal)
+ printf("The given number is palindrome\n");
+ else
+ printf("The given number is not palindrome\n");
+}
