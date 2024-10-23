bbde95ceec04d57055150441abc58fa1c12be2b7
SahuAyush26 <ayush.sahu24@spit.ac.in>
2024-10-23 19:26:13 +0530

    wrote the whole program of palindrome from scratch

--- palindrome.c
+++ palindrome.c
@@ -1,2 +1,31 @@
 //C program to check a number whether it is palindrome or not.
 
+#include <stdio.h>
+
+int main(void)
+{
+    int x, og, rv=0, rm;
+    printf("Enter a integer. ");
+    scanf("%i", &x);
+
+    og = x;
+
+    for(int i=1; i>0; i++)
+    {
+        rm = x%10;
+        rv = rv*10 + rm;
+        x /= 10;
+
+        if(x==0){
+            break;
+        }
+    }
+    if(rv == og){
+        printf("number is palindrome");
+    }
+    else{
+        printf("number is not palindrome");
+    }
+    
+    return 0;
+}
