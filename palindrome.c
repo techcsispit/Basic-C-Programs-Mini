7d2611dfc55b1703dfe0896d1195c901fa70ffc5
Rishmore07 <morerishika07@gmail.com>
2024-10-23 19:22:59 +0530

    to check the number is palindrome or not

--- palindrome.c
+++ palindrome.c
@@ -1,2 +1,23 @@
-//C program to check a number whether it is palindrome or not.
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number to check wheter it is palindrome num or not");
+scanf("%d",&n);
+int rev;
+int org=n;
+while (n!=0)
+{
+    int i=n%10;
+    rev=rev*10+i;
+    n=n/10;
+
+
+
+}
+if(org==rev)
+printf("the number is palindrome number");
+else
+printf("the number is not palindrome number");
+}
 
