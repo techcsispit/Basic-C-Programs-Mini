d930948f3be4da18920914b0d1f2fc04e6b9e913
thedevanshimehta <dvanshi613@gmail.com>
2024-10-23 19:29:06 +0530

    cpp file for primesearch

--- primesearch.c
+++ primesearch.c
@@ -2,5 +2,26 @@
 #include <stdio.h>
 int main()
 {
-    
+    int n; int s=0;
+    print("Enter the no :");
+    scanf("%d",&n);
+    if(n==1|| n==0 ){
+        printf("Neither prime nor composite");
+    }
+    else if(n==2){
+        printf("prime number");
+    }
+    else{
+    for (int i = 2; i<n; i++){
+        if(n%i==0){
+          s++;
+        }
+    }
+    if(s==0){
+        printf("Prime number");
+    }
+    else{
+        printf("Composite number");
+    }}
+    return 0;
 }
