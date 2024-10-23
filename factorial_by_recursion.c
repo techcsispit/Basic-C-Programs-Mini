285331b9f4e4c509f60331a12876d08441bca5b8
het salot <hetsalot1410@gmail.com>
2024-10-23 19:37:54 +0530

    factorial by recurssion

--- factorial_by_recursion.c
+++ factorial_by_recursion.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-long fact(int n){
-
+int fact(int n){
+    if(n==0)
+    {
+        return 1;
+    }
     return n *fact(n-1);
 }
 int main(){
--- print_number_Line_by_Line_Sum_and_total.c
+++ print_number_Line_by_Line_Sum_and_total.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #define max 100
-
+#include <math.h>
 //Function to print digit numbers line by line.
 //Like :- User enter the number : 123
 //output be like :- 1
 //                  2
 //                  3
 void line_by_line(int no){
-    int c;
+  
 }
 
 //Function to print Sum of the entered digit.
 // Example :- 123 = 6
 void sum_digit(int no){
+    int t;
+    int sum=0;
+    while(no>0)
+    {
+        t=no%10;
+        sum=sum+t;
+        no=no/10;
+    }
     
 }
 
