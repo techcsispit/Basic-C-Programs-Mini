487247968f6d3105ce1a052ebfc424200217c0df
Vatsal Panchal <vatsalpanchal2006@gmail.com>
2024-10-23 19:25:02 +0530

    This code checks if the inputted number is prime or not.

--- primesearch.c
+++ primesearch.c
@@ -1,6 +1,33 @@
-//To check the n no. is a prime no. or not
-#include <stdio.h>
+#include<stdio.h>
+
 int main()
 {
-    
+	int flag=1,num;
+	
+	printf("Enter a number:");
+	scanf("%d", &num);
+	
+	if(num==1)
+	{
+		printf("1 is neither prime nor composite.");
+	}
+	
+	for(int i=2;i*i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			flag=0;
+			break;
+		}
+	}
+	if(flag==0)
+	{
+		printf("%d is not a prime number.",num);
+	}
+	else
+	{
+		printf("%d is a prime number.",num);
+	}
+	
+	return 0;
 }
