9f91593999fccc799aa530e9b49705f5c92f7987
SahuAyush26 <ayush.sahu24@spit.ac.in>
2024-10-23 19:23:03 +0530

    wrote the whole program of counting digits, from scratch.

--- countdigit.c
+++ countdigit.c
@@ -1,1 +1,21 @@
 //C program to count number of digits in a number.
+#include <stdio.h>
+
+int main(){
+    int a;
+    int og = a;
+    int rem;
+    int count = 0;
+    printf("Enter your digit : ");
+    scanf("%d", &a);
+    for(int i = 1; i>0; i*=10){
+        if(a/i != 0){
+            count += 1;
+        }
+        else{
+            break;
+        }
+    }
+    printf("Number of digits are %d ", count);
+    return 0;
+}
