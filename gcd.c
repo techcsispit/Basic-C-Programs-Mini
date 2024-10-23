c07c234b9cd45451970c9fa04d5d611eb3ace9c3
het salot <hetsalot1410@gmail.com>
2024-10-23 18:51:16 +0530

    GCD

--- gcd.c
+++ gcd.c
@@ -4,8 +4,16 @@
 #define s scanf(
 void gcd(int a,int b)
 {
-    // complete code
-    
+    int i;
+    int gcd=1;
+    for(i=1;i<a && i<b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    printf("GCD is:%d",i);
 }
 int main()
 {
