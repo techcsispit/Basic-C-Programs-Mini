fa6ef6f6433fc86187fb8a23f2ccc830c0fa240d
SahuAyush26 <ayush.sahu24@spit.ac.in>
2024-10-23 19:24:35 +0530

    wrote the whole program of finding factors from scratch

--- factors.c
+++ factors.c
@@ -1,1 +1,20 @@
 //C program to find factors of any inputted number.
+#include <stdio.h>
+
+int main (void)
+{
+    int x;
+    printf("Enter a number. ");
+    scanf("%d", &x);
+
+    printf("The factors of %i are ", x);
+    for(int i=1; i<=x; i++)
+    {
+        if(x%i==0)
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
