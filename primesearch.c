b3ff3f1ff27d2ee14a47d0d39c854e6390dd79cb
prashamgosar <prashamgosar@gmail.com>
2024-10-23 19:36:15 +0530

    commit

--- primesearch.c
+++ primesearch.c
@@ -1,6 +1,26 @@
 //To check the n no. is a prime no. or not
-#include <stdio.h>
-int main()
+#include<stdio.h>
+
+void prime(int a)
 {
-    
+	int count = 0;
+	for(int i = 1; i<=a ; i++)
+	{
+		if(a%i==0)
+		{
+	  		count++;
+		}
+	}
+	if(count==2)
+		printf("Its prime\n");
+	else
+		printf("Its not prime\n");
+}
+
+void main()
+{
+	int p;
+	printf("Enter a number");
+	scanf("%d",&p);
+	prime(p);
 }
