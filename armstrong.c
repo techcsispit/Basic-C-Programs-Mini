82c209d6d08411d8fea9edd5ae3b15e0b73673d0
thedevanshimehta <dvanshi613@gmail.com>
2024-10-23 19:20:53 +0530

    cpp file for armstrong

--- armstrong.c
+++ armstrong.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include<math.h>
 int main()
 {
     // complete the code
+    int number;
+    printf("Enter a number : ");
+    scanf("%d",&number);
+    int s=0; int a; int b; int c;int x; int count=0;
+    a=number; b=number; c=number;
+    do{
+      a=number%10;
+      number=number/10;
+      s++;
+    }while(number!=0);
+    do{
+        x=b%10;
+        b=b/10;
+        count=count + pow(x,s);
+    }while(b!=0);
+    if(count==c){
+        printf("Armstrong number");
+    }
+    else{
+        printf("Not an armstrong number");
+    }
 
     return 0;
 }
