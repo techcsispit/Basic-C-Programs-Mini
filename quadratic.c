576cba9d48cb41262ffd6cb63e3e971076a1709e
kawakiGG <dshivaji532@gmail.com>
2024-10-23 18:27:47 +0530

    points dede plsplsplsplslpspls

--- quadratic.c
+++ quadratic.c
@@ -1,1 +1,28 @@
-//C program  to find roots of a quadratic equation.
+#include<stdio.h>
+#include<math.h>
+int main()
+{   
+    int a,b,c;
+    printf(" ax^2 +bx +c=0\n");
+    printf("Enter a in this format\n");
+    scanf("%d",&a);
+    printf("Enter b in this format\n");
+        scanf("%d",&b);
+    printf("Enter c in this format\n");
+        scanf("%d",&c);
+    int det;
+    int root1,root2;
+    det=b*b-4*a*c;
+    if(det<0){
+        det=det*(-1);
+        printf("The roots are imaginary");
+        //not tryna find the img roots rn
+    }else
+    {
+        root1= (-b + sqrt(det))/2*a;
+        root2= (-b - sqrt(det))/2*a;
+        printf("The roots are %d %d",root1,root2);
+    }
+    
+    return 0;
+}
