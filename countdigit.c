89703a4f66a47e182a739e0440478988115fabce
thedevanshimehta <dvanshi613@gmail.com>
2024-10-23 19:24:29 +0530

    cpp file for countdigit

--- countdigit.c
+++ countdigit.c
@@ -1,1 +1,19 @@
 //C program to count number of digits in a number.
+#include <stdio.h>
+
+int main()
+{ 
+    int number;
+    printf("Enter a number : ");
+    scanf("%d",&number);
+    int s=0; int a; 
+    a=number;
+    do{
+        a=number%10;
+        number=number/10;
+        s++;
+    }while(number!=0);
+    printf("Number of digits : %d",s);
+    return 0;
+    
+}
