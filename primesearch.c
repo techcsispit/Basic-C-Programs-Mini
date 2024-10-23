60ef5c117a40ea4a0d41b11357abc36c705fe8b8
het salot <hetsalot1410@gmail.com>
2024-10-23 19:13:50 +0530

    prime

--- primesearch.c
+++ primesearch.c
@@ -2,5 +2,32 @@
 #include <stdio.h>
 int main()
 {
-    
+    int i,n,flag;
+    flag=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("Neither prime nor compostie");
+    }
+    else
+    {
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("The number %d is prime.",n);
+    }
+    else
+    {
+        printf("The numbe is not prime umber.");
+    }
+    }
+    return 0;
 }
