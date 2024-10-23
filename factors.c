1212ab0c7a122708f9c9e74ad7019b4901525da7
het salot <hetsalot1410@gmail.com>
2024-10-23 18:43:31 +0530

    factors

--- factors.c
+++ factors.c
@@ -1,1 +1,14 @@
 //C program to find factors of any inputted number.
+int main()
+{
+    int n,i;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+}
